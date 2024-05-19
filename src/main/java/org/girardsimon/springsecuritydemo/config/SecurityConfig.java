179d2bba20273c5697c6b4faa242b49b74af1b59
Simon Girard <simon.girard1991@gmail.com>
2024-05-19 13:15:01 +0200

    Introduce OAuth2 login with Google

--- src/main/java/org/girardsimon/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/org/girardsimon/springsecuritydemo/config/SecurityConfig.java
@@ -2,13 +2,14 @@ package org.girardsimon.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +23,8 @@ public class SecurityConfig {
                     auth.requestMatchers("/favicon.ico").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .formLogin(Customizer.withDefaults())
+                .formLogin(withDefaults())
+                .oauth2Login(withDefaults())
                 .build();
     }
 
--- src/main/java/org/girardsimon/springsecuritydemo/controller/WebController.java
+++ src/main/java/org/girardsimon/springsecuritydemo/controller/WebController.java
@@ -1,7 +1,11 @@
 package org.girardsimon.springsecuritydemo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class WebController {
@@ -12,7 +16,15 @@ public class WebController {
     }
 
     @GetMapping("/private")
-    public String privatePage() {
-        return "Hello Private! 🐯";
+    public String privatePage(Authentication authentication) {
+        return "Hello Private! 🐯" + getName(authentication);
+    }
+
+    private static String getName(Authentication authentication) {
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getEmail)
+                .orElseGet(authentication::getName);
     }
 }
